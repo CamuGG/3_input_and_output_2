4eb1a50e2959a005507489a98d94327c0f240da0
Camu <simone.benedetti94@gmail.com>
2023-07-11 20:17:45 +0200

    add the second exercise to primitive

---
+++ src/Input_and_output_2/PrintYourNameAndSurname.java
@@ -0,0 +1,8 @@
+package Input_and_output_2;
+
+public class PrintYourNameAndSurname {
+    public static void main(String[] args) {
+        System.out.print("Simone ");
+        System.out.print("Benedetti");
+    }
+}
